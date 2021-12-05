01d7fa343e65ac94c9771a0eec3a93631bf81faf
PC <pc@MacBook-Air-de-PC.local>
2021-12-05 20:23:34 +0100

    Ajout de la page d'acceuil avec les photos tendance

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, Directive, Input, OnInit } from '@angular/core';
+import { PhotosInterface } from '../photos-interface';
 
 @Component({
   selector: 'app-result',
@@ -9,12 +11,20 @@ export class ResultComponent implements OnInit {
 
   searchPhotoUrls : Array<String> = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    let url = "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key="
+    url = url + "3916944f55d2c8f5a6f3c7e2b0c37ccb" + "&format=json&nojsoncallback=1"; 
+    this.http.get<PhotosInterface>(url).subscribe(data => {
+      this.displayResultImages(data.photos.photo);
+    }, (err: HttpErrorResponse) => {
+      console.log("An error has occured");
+    }); 
   }
 
-  displayResultImages(photos : Array<any>){ 
+  displayResultImages(photos : any){ 
+    this.searchPhotoUrls = []; // clean old photos
     if(photos.length != 0){
       console.log(photos);
       for(let i = 0; i < photos.length; i++){
